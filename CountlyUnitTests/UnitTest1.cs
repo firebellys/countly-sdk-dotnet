e62465cab0f9d68e7dd46aea7a503e6cba38b350
Erik Umenhofer <firebelly@gmail.com>
2015-10-20 14:33:40 -0700

    Added Unit Test for testing.

---
+++ CountlyUnitTests/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CountlySDK;
+
+namespace CountlyUnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            try
+            {
+                Countly.StartSession("http://cloud.count.ly", "87e43ddcc40b5cd0d4ac857a71d250e1750847f0");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+    }
+}
